f176305d96d19eb3cce413479ca2fe5c1ee7702b
Federico Olive <FedericoOlive@gmail.com>
2022-03-29 22:45:37 -0300

    Change move units to Rigidbody.

--- Assets/Scripts/Units/UnitStateMoving.cs
+++ Assets/Scripts/Units/UnitStateMoving.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 public class UnitStateMoving : MonoBehaviour
 {
     private Unit unit;
+    [SerializeField] private Rigidbody rb;
 
     private void Awake()
     {
         unit = GetComponent<Unit>();
+        rb.GetComponent<Rigidbody>();
     }
     private void Update()
     {
-        transform.position += new Vector3(unit.stats.velocity * Time.deltaTime * unit.signDirection, 0, 0);
+        Vector3 move = new Vector3(unit.stats.velocity * Time.deltaTime * unit.signDirection, 0, 0);
+        rb.MovePosition(rb.position + move);
     }
 }
