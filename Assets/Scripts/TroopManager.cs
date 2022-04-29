6650823655ee79ed8c41fb8e095e3add462b9a44
Sauron <federicoolive@gmail.com>
2022-04-29 20:05:07 -0300

    Add life fade in out. #111

--- Assets/Scripts/TroopManager.cs
+++ Assets/Scripts/TroopManager.cs
@@ -9,7 +9,7 @@ public class TroopManager : MonoBehaviour
     [SerializeField] private int layerToTroop;
     [SerializeField] private LayerMask layerToInteract;
     [SerializeField] private bool unitsGoToRight = true;
-    [SerializeField] private GameObject[] prefabUnits;
+    [SerializeField] private GameObject prefabUnits;
     [SerializeField] private List<Unit> unitsAlive;
 
     public void OnButtonCreateTroop(int tropIndex)
@@ -17,21 +17,26 @@ public class TroopManager : MonoBehaviour
         Vector3 startPos = startPosition.position;
         Vector3 endPos = endPosition.position; 
 
-        if (!prefabUnits[tropIndex])
+        if (!prefabUnits)
         {
-            Debug.LogWarning("No existe el indce de esa tropa!!");
+            Debug.LogWarning("No existe el indice de esa tropa!!");
             return;
         }
 
         float random = Random.Range(0, 1.0f);
         Vector3 pos = Vector3.Lerp(startPos, endPos, random);
 
-        GameObject unitGameObject = Instantiate(prefabUnits[tropIndex], pos, Quaternion.identity, transform);
+        GameObject unitGameObject = Instantiate(prefabUnits, pos, Quaternion.identity, transform);
         unitGameObject.gameObject.layer = layerToTroop;
         Unit unit = unitGameObject.GetComponent<Unit>();
         unit.signDirection = unitsGoToRight ? 1 : -1;
         unit.layerMaskInteraction = layerToInteract;
         unitsAlive.Add(unit);
+
+        int indexImage = (int)GameManager.Get().unitsStatsLoaded[tropIndex].tempCurrentShape;                                  // Temporal
+        unitGameObject.GetComponent<MeshFilter>().mesh = GameManager.Get().GetCurrentMesh(indexImage);                         // Temporal
+        unitGameObject.GetComponent<MeshRenderer>().material.color = GameManager.Get().unitsStatsLoaded[tropIndex].tempColor;  // Temporal
+
 
         UnitStats unitStats = GameManager.Get().GetUnitStats(tropIndex);
         unit.SetValues(unitStats);
--- Assets/Scripts/UI/UiComponentLife.cs
+++ Assets/Scripts/UI/UiComponentLife.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,12 +7,48 @@ public class UiComponentLife : MonoBehaviour
 {
     [SerializeField] private Unit unit;
     [SerializeField] private Image imageFill;
+
+    private CanvasGroup canvasGroup;
+    private float showingTime = 5f;
+    private float transparencyTime = 1f;
+    private IEnumerator HideProcessCor;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
     private void Start()
     {
         unit.OnTakeDamage += TakeDamageUI;
     }
+
     void TakeDamageUI(float currentLife, float maxLife)
     {
+        ShowPanel();
         imageFill.fillAmount = currentLife / maxLife;
+    }
+
+    void ShowPanel()
+    {
+        canvasGroup.alpha = 1;
+
+        if (HideProcessCor != null)
+            StopCoroutine(HideProcessCor);
+
+        HideProcessCor = HideProcess();
+        StartCoroutine(HideProcessCor);
+    }
+
+    IEnumerator HideProcess()
+    {
+        yield return new WaitForSeconds(showingTime);
+        float onTime = 0;
+        while (onTime < transparencyTime)
+        {
+            onTime += Time.deltaTime;
+            canvasGroup.alpha = 1 - onTime / transparencyTime;
+            yield return null;
+        }
     }
 }
