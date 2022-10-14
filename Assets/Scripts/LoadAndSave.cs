f80634f579e02c0c7f5edde12b3335f73595460e
Adrian Sgro <Owinmowe@gmail.com>
2022-10-14 17:21:32 -0300

    Hotfix data not loading correctly in android (txt in resources)

--- Assets/Scripts/LoadAndSave.cs
+++ Assets/Scripts/LoadAndSave.cs
@@ -1,31 +1,59 @@
 using System.IO;
 using UnityEngine;
 //using Newtonsoft.Json;
+
 public class LoadAndSave
 {
-    public static string LoadFromFile(string filename)
+
+    public static string LoadFromFile(string filename, bool playerPref = false)
     {
+        if (playerPref)
+        {
+            if (PlayerPrefs.HasKey(filename))
+            { 
+                return PlayerPrefs.GetString(filename);
+            }
+        }
+        
         TextAsset textAsset = (TextAsset)Resources.Load(filename);
         if (textAsset == null) return null;
         string stringData = textAsset.text;
         return stringData;
     }
-    public static void SaveToFile(string filename, string stringData)
+
+    public static void SaveToFile(string filename, string stringData, bool playerPref = false)
     {
-        string path = Application.dataPath + "/Resources/" + filename + ".txt";
-        File.WriteAllText(path, stringData);
-        
+        if (playerPref)
+        {
+            PlayerPrefs.SetString(filename, stringData);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            string path = Application.dataPath + "/Resources/" + filename + ".txt";
+            File.WriteAllText(path, stringData);
+
 #if UNITY_EDITOR
-        UnityEditor.AssetDatabase.Refresh();
+            UnityEditor.AssetDatabase.Refresh();
 #else
-        File.WriteAllText(filename + ".txt", stringData);
+                File.WriteAllText(filename + ".txt", stringData);
 #endif
+        }
     }
-    public static void OverwriteResourceFile(string filename)
+
+    public static void OverwriteResourceFile(string filename, bool playerPref = false)
     {
         string path = filename + ".txt";
         string data = File.ReadAllText(path);
 
-        SaveToFile(filename, data);
+        if (playerPref)
+        {
+            PlayerPrefs.SetString(filename, data);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            SaveToFile(filename, data, true);
+        }
     }
 }
