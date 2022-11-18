1b18b10697a8f3adf0847d0ef403f5b960d7ee66
Adrian Sgro <Owinmowe@gmail.com>
2022-11-18 19:37:04 -0300

    Add enemy material for forest enemy

--- Assets/Scripts/Gameplay/Enemies/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemies/Enemy.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private EnemyType enemyType = default;
-
+    [SerializeField] private Renderer[] renderers;
+    [SerializeField] private Material[] materials;
     public Unit Unit { get; private set; } = default;
 
     public int EnemyIndex { get; set; }
@@ -23,6 +24,14 @@ public class Enemy : MonoBehaviour
         config.AttackLaneFlags = Unit.AttackLaneFlags;
         config.OwnLaneFlags = Unit.OwnLaneFlags;
         return config;
+    }
+
+    public void SetRenderers(int terrainIndex)
+    {
+        foreach (var rend in renderers)
+        {
+            rend.material = materials[terrainIndex];
+        }
     }
     
 }
