18a4f6d924e440ec1d22c1949be1102e26df2c44
Adrian Sgro <Owinmowe@gmail.com>
2022-12-04 18:48:29 -0300

    Fix post level breaking in some dead units cases

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 public class Utils
 {
     public static bool LayerEquals(LayerMask mask, int layer)
@@ -15,5 +16,38 @@ public class Utils
         List<RaycastResult> results = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
         return results.Count > 0;
+    }
+}
+
+public static class ListExtensions 
+{
+    public static void RemoveAllIndices<T>(this List<T> list, IEnumerable<int> indices)
+    {
+        //do not remove Distinct() call here, it's important
+        var indicesOrdered = indices.Distinct().ToArray();
+        if(indicesOrdered.Length == 0)
+            return;
+
+        Array.Sort(indicesOrdered);
+
+        if (indicesOrdered[0] < 0 || indicesOrdered[indicesOrdered.Length - 1] >= list.Count)
+            throw new ArgumentOutOfRangeException();
+
+        int indexToRemove = 0;
+        int newIdx = 0;
+
+        for (int originalIdx = 0; originalIdx < list.Count; originalIdx++)
+        {
+            if(indexToRemove < indicesOrdered.Length && indicesOrdered[indexToRemove] == originalIdx)
+            {
+                indexToRemove++;
+            }
+            else
+            {
+                list[newIdx++] = list[originalIdx];
+            }
+        }
+
+        list.RemoveRange(newIdx, list.Count - newIdx);
     }
 }
