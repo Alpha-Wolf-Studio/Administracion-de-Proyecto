65b1ebec1b49b8dd9fa8d2c70714ac303b0a0291
Adrian Sgro <Owinmowe@gmail.com>
2022-12-04 19:11:24 -0300

    Modify options scene adding credits scene

--- Assets/Scripts/UI/UiPanelSettings.cs
+++ Assets/Scripts/UI/UiPanelSettings.cs
@@ -3,15 +3,26 @@ using UnityEngine.UI;
 
 public class UiPanelSettings : MonoBehaviour
 {
+    [Header("Buttons")]
     [SerializeField] private Button btnBack;
+    [SerializeField] private Button btnCredits;
+    [SerializeField] private Button btnCreditsBack;
     [SerializeField] private Button btnMusicOnOff;
     [SerializeField] private Button btnEffectOnOff;
+
+    [Header("Panels")] 
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject creditsPanel;
+    
+    [Header("Sprite References")]
     [SerializeField] private Sprite spriteAudioOn;
     [SerializeField] private Sprite spriteAudioOff;
 
     private void Start()
     {
         btnBack.onClick.AddListener(OnPressBack);
+        btnCredits.onClick.AddListener(OnPressCredits);
+        btnCreditsBack.onClick.AddListener(OnPressCreditsBack);
 
         btnMusicOnOff.image.sprite = AudioManager.Get().isMusicOn ? spriteAudioOn : spriteAudioOff;
         btnEffectOnOff.image.sprite = AudioManager.Get().isEffectOn ? spriteAudioOn : spriteAudioOff;
@@ -26,6 +37,18 @@ public class UiPanelSettings : MonoBehaviour
         Time.timeScale = 1;
     }
 
+    private void OnPressCredits()
+    {
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    private void OnPressCreditsBack()
+    {
+        settingsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+    
     private void EnableMusic()
     {
         AudioManager.Get().EnableMusic();
